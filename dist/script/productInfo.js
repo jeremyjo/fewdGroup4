f73a8401def309cf874123790b5225a1ebf302af
DianaDi777 <tikhonovskova@gmail.com>
2020-05-27 18:29:37 -0700

    costomized Product Card

--- dist/script/productInfo.js
+++ dist/script/productInfo.js
@@ -1,28 +1,30 @@
-const colorChoice = document.querySelectorAll(".color");
-const products = document.querySelectorAll(".product");
+//colors shifts images
+
+const colorChoicePI = document.querySelectorAll(".colorPI");
+const productspi = document.querySelectorAll(".productPI");
 
 function changeColor() {
-  let primary = this.getAttribute("primary");
+  //   let primary = this.getAttribute("primary");
   let color = this.getAttribute("color");
-  let product = document.querySelector(`.product[color="${color}"]`);
-  let gradient = document.querySelector(`.gradient[color="${color}"]`);
+  let productpi = document.querySelector(`.productPI[color="${color}"]`);
+  //   let gradient = document.querySelector(`.gradient[color="${color}"]`);
 
-  colorChoice.forEach((c) => c.classList.remove("active"));
-  this.classList.add("active");
+  colorChoicePI.forEach((c) => c.classList.remove("activePI"));
+  this.classList.add("activePI"); //color-buttons changes
 
   document.documentElement.style.setProperty;
 
-  products.forEach((s) => s.classList.remove("show"));
-  product.classList.add("show");
+  productspi.forEach((s) => s.classList.remove("showPI"));
+  productpi.classList.add("showPI");
 
-  gradients.forEach((g) => g.classList.remove("first"));
-  gradient.classList.add("first");
+  //   gradients.forEach((g) => g.classList.remove("firstPI"));
+  //   gradient.classList.add("firstPI");
 }
 
-colorChoice.forEach((c) => c.addEventListener("click", changeColor));
+colorChoicePI.forEach((c) => c.addEventListener("click", changeColor));
 
 function teaCost() {
-  num1 = document.getElementById("amount").value;
-  num2 = 12;
-  document.getElementById("total").innerHTML = num1 * num2;
+  num1 = document.getElementById("amountPI").value;
+  num2 = 10;
+  document.getElementById("totalPI").innerHTML = num1 * num2;
 }
