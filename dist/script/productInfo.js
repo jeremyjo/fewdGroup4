3712b5f33d76b47a7bd77bec7c9ac4605259a64a
DianaDi777 <tikhonovskova@gmail.com>
2020-05-27 20:37:12 -0700

    final product info

--- dist/script/productInfo.js
+++ dist/script/productInfo.js
@@ -4,10 +4,8 @@ const colorChoicePI = document.querySelectorAll(".colorPI");
 const productspi = document.querySelectorAll(".productPI");
 
 function changeColor() {
-  //   let primary = this.getAttribute("primary");
   let color = this.getAttribute("color");
   let productpi = document.querySelector(`.productPI[color="${color}"]`);
-  //   let gradient = document.querySelector(`.gradient[color="${color}"]`);
 
   colorChoicePI.forEach((c) => c.classList.remove("activePI"));
   this.classList.add("activePI"); //color-buttons changes
@@ -16,9 +14,6 @@ function changeColor() {
 
   productspi.forEach((s) => s.classList.remove("showPI"));
   productpi.classList.add("showPI");
-
-  //   gradients.forEach((g) => g.classList.remove("firstPI"));
-  //   gradient.classList.add("firstPI");
 }
 
 colorChoicePI.forEach((c) => c.addEventListener("click", changeColor));
